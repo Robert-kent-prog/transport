5d2087ec3c79db59272363274c4327880c0fe869
abel254-lang <abelkisang57@gmail.com>
2025-03-30 20:37:28 +0300

    Added the verify JWt middleware in the stk push and the b2c endpoints.

--- backend/routes/MpesaTokenRoute.js
+++ backend/routes/MpesaTokenRoute.js
@@ -6,15 +6,18 @@ import {
     b2cPayment,
     simulator
 } from '../controllers/MpesaToken.js';
+
+import { verifyJWT } from '../middleware/verifyJWT.js';
+
 const router = express.Router();
 // Route to get token and trigger STK Push
-router.post('/stkpush', createToken, stkPush);
+router.post('/stkpush', verifyJWT, createToken, stkPush);
 
 // Route to handle Validation callback from M-Pesa
-router.post('/simulate', createToken, simulator);
+router.post('/simulate', verifyJWT, createToken, simulator);
 
 //route for b2c payment
-router.post('/b2c', createToken, b2cPayment);
+router.post('/b2c', verifyJWT, createToken, b2cPayment);
 
 
 export default router;
